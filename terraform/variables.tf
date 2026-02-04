48822dc1fbfdbe42e4bd17a671171bf332925cf5
desimetallica <ddesi89@gmail.com>
2026-02-04 17:08:02 +0100

    privilege escalation lab with developer user

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,3 +29,44 @@ variable "ssh_private_key_path" {
   description = "Path to your local SSH private key file (e.g., ~/.ssh/id_rsa)"
   type        = string
 }
+
+variable "s3_workload_bucket_name" {
+  description = "Name of the S3 bucket to be created"
+  type        = string
+}
+
+variable "s3_cloudtrail_bucket_name" {
+  description = "Name of the S3 bucket for CloudTrail logs"
+  type        = string
+}
+
+variable "admin_user_name" {
+  description = "Name of the admin IAM user"
+  type        = string
+}
+
+variable "developer_user_name" {
+  description = "Name of the developer IAM user"
+  type        = string
+}
+
+variable "readonly_user_name" {
+  description = "Name of the readonly IAM user"
+  type        = string
+}
+
+variable "admin_group_name" {
+  description = "Name of the admin IAM group"
+  type        = string
+}
+
+variable "developer_group_name" {
+  description = "Name of the developer IAM group"
+  type        = string
+}
+
+variable "readonly_group_name" {
+  description = "Name of the readonly IAM group"
+  type        = string
+}
+
